17c230adc1cc9650e3f705ddb81a30b6ae9ed8a1
raulvictorvieira <raulvictorsv@gmail.com>
2021-11-03 23:13:32 -0300

    modulos

---
+++ javascript/modulos/main.mjs
@@ -0,0 +1,5 @@
+import { mostraIdade, mostraCidade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade('Raul', 26));
+console.log(mostraCidade('Raul', 'Curitiba'));
+console.log(mostraHobby('Raul', 'Games'));
